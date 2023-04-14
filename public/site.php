2717edc6e0f6fdd26064a30b127a1fc327e92ede
eye <eye@frog.company>
2023-04-14 12:15:31 +0900

    update

--- public/site.php
+++ public/site.php
@@ -1,6 +1,31 @@
 <?php
 
+?>
+<html>
+<head>
+    <title>OshiMe.site</title>
+</head>
+<body>
+<?php
 
-phpinfo();
+print "<h1>".$_SERVER['SERVER_NAME']."</h1>";
+?>
+<form action="" method="post">
+    <br />
+    <?php
+    if (trim(urlencode($_POST["wallet"]))!=""){
+        ?>
+        <input type="submit" name="google" value="Google Connect"><br />
+        <input type="submit" name="amazon" value="Amazon Connect"><br />
+    <?php
+    }else{
+?>
+    <input type="submit" name="wallet" value="Wallet Login"><br />
+<?php
+    }
+    ?>
 
+</form>
 
+</body>
+</html>
