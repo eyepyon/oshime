a30cbc00e00cb0db3a53d359729e785f07a8a90e
eye <eye@frog.company>
2023-04-14 12:49:25 +0900

    update

--- public/site.php
+++ public/site.php
@@ -17,6 +17,14 @@ print "<h1>".$_SERVER['SERVER_NAME']."</h1>";
         ?>
         <input type="submit" name="google" value="Google Connect"><br />
         <input type="submit" name="amazon" value="Amazon Connect"><br />
+        <input type="hidden" name="wallet" value="<?php print trim(urlencode($_POST["wallet"])); ?>"><br />
+        <br />
+        <textarea>
+        </textarea>
+        BackGround color:<input type="radio">White　<input type="radio">Dark　<input type="radio">Fancy<br />
+
+
+        <input type="submit" value="update">
     <?php
     }else{
 ?>
