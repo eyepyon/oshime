ee21d13cec08ce2a27f08d6aa580055080baa2ad
eye <eye@frog.company>
2023-04-14 13:17:52 +0900

    update

--- public/site.php
+++ public/site.php
@@ -17,20 +17,22 @@ print "<h1>".$_SERVER['SERVER_NAME']."</h1>";
     $url2 = trim($_POST["url2"]);
     $url3 = trim($_POST["url3"]);
     $wallet = trim(urlencode($_POST["wallet"]));
+    $memo = trim($_POST["memo"]);
+    $BG = trim($_POST["BG"]);
     if ($wallet!=""){
         ?>
         URL1:<input type="text" name="url1" value="<?php print $url1; ?>"><br />
-        URL2:<input type="text" name="url2" value="<?php print $url1; ?>"><br />
-        URL3:<input type="text" name="url3" value="<?php print $url1; ?>"><br />
+        URL2:<input type="text" name="url2" value="<?php print $url2; ?>"><br />
+        URL3:<input type="text" name="url3" value="<?php print $url3; ?>"><br />
         <br />
         <input type="submit" name="google" value="Google Connect"><br />
         <input type="submit" name="amazon" value="Amazon Connect"><br />
-        <input type="hidden" name="wallet" value="<?php print trim(urlencode($_POST["wallet"])); ?>"><br />
+        <input type="hidden" name="wallet" value="<?php print $wallet; ?>"><br />
         <br />
-        <textarea>
-        </textarea>
-        BackGround color:<input type="radio">White　<input type="radio">Dark　<input type="radio">Fancy<br />
-
+        <textarea name="memo"><?php print $memo ?></textarea>
+        BackGround color:
+        <input type="radio" name="BG" <?php if($BG == "White"){print " checked";} ?>>White　
+        <input name="BG" type="radio" <?php if($BG == "Dark"){print " checked";} ?>>Dark<br />
 
         <input type="submit" value="update">
     <?php
