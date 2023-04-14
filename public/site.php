c17a1415c647d679e96cbcba6ce087f170d5d776
eye <eye@frog.company>
2023-04-14 13:01:53 +0900

    update

--- public/site.php
+++ public/site.php
@@ -13,8 +13,16 @@ print "<h1>".$_SERVER['SERVER_NAME']."</h1>";
 <form action="" method="post">
     <br />
     <?php
-    if (trim(urlencode($_POST["wallet"]))!=""){
+    $url1 = trim($_POST["url1"]);
+    $url2 = trim($_POST["url2"]);
+    $url3 = trim($_POST["url3"]);
+    $wallet = trim(urlencode($_POST["wallet"]));
+    if ($wallet!=""){
         ?>
+        URL1:<input type="text" name="url1" value="<?php print $url1; ?>"><br />
+        URL2:<input type="text" name="url2" value="<?php print $url1; ?>"><br />
+        URL3:<input type="text" name="url3" value="<?php print $url1; ?>"><br />
+        <br />
         <input type="submit" name="google" value="Google Connect"><br />
         <input type="submit" name="amazon" value="Amazon Connect"><br />
         <input type="hidden" name="wallet" value="<?php print trim(urlencode($_POST["wallet"])); ?>"><br />
